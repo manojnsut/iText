da50628f0d6bacc0f2072a51551144c0c6017159
Manoj Kumar <manojbagheleinstein@gmail.com>
2022-06-30 16:10:33 +0530

    ui updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'iText Bot';
+const botName = 'iText ';
 
 // Run when client connects
 io.on('connection', socket => {
